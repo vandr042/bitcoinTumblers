ceded4802a55e23ff0e7610a0dd4805bcf619b82
Connor Van Drisse <vandr042@umn.edu>
2015-09-30 13:11:50 -0500

    Links addresses across transactions

--- bitcoinT/src/bitcoinLink/Link.java
+++ bitcoinT/src/bitcoinLink/Link.java
@@ -1,4 +1,4 @@
-package bitcoinT;
+package bitcoinLink;
 
 import org.bitcoinj.core.*;
 import org.bitcoinj.kits.WalletAppKit;
@@ -7,6 +7,8 @@ import org.bitcoinj.store.*;
 import org.bitcoinj.utils.BriefLogFormatter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
@@ -17,8 +19,10 @@ public class Link {
 	 * @throws ExecutionException 
 	 * @throws InterruptedException 
 	 * @throws BlockStoreException 
+	 * @throws FileNotFoundException 
+	 * @throws AddressFormatException 
 	 */
-	public static void main(String[] args) throws InterruptedException, ExecutionException, BlockStoreException {
+	public static void main(String[] args) throws InterruptedException, ExecutionException, BlockStoreException, FileNotFoundException, AddressFormatException {
 		NetworkParameters params = MainNetParams.get();
 		WalletAppKit kit = new WalletAppKit(params, new File("foo"), "test");
 		kit.startAsync();
@@ -27,13 +31,24 @@ public class Link {
 		Peer dlPeer = pGroup.getDownloadPeer();
 		BlockChain chain = kit.chain();
 		StoredBlock stored_block = chain.getChainHead();
-		
 		HashMap<Address,LinkedList<Address>> cluster_map = new HashMap();
 		
-		/* Increases counter to scale number of transactions */
-		for (int counter = 10; counter < 11; counter++) {
+		File file2 = new File("clusters.txt");
+		PrintWriter hwriter = new PrintWriter("clusters.txt");
+
+		/* COUNTERS */
+		int mergeCount = 0;
+		int largestCluster = 0;
+		double eCount = 0.0;
+		double totalTx = 0.0;
+		
+		
+		/* Increase counter to scale blocks fetched */
+		for (int counter = 10; counter < 12; counter++) {
 			Block tBlock = dlPeer.getBlock(stored_block.getHeader().getHash()).get();
 			List<Transaction> tx_list = tBlock.getTransactions();
+			System.out.println("has: " + tx_list.size() + "transactions"); 
+			totalTx += tx_list.size();
 			for (Transaction tx : tx_list) {
 				List<TransactionInput> tx_ilist = tx.getInputs();
 				if (tx_ilist.size() > 1) {
@@ -46,6 +61,7 @@ public class Link {
 					 */
 					
 					boolean cluster_in_map = false;
+					boolean exc = false;
 					for (TransactionInput tx_input : tx_ilist) {
 						try{
 							LinkedList<Address> a_list = cluster_map.get(tx_input.getFromAddress());
@@ -55,52 +71,89 @@ public class Link {
 								//loop over inputs to check if already in address array 
 								for (TransactionInput txi : tx_ilist){
 									Address addr = txi.getFromAddress();
-									Address current = a_list.getFirst();
 								
 									boolean found = false;
 									for (int i = 0; i < a_list.size(); i++){ //loop through list checking for addr break if found
-										if (addr == current){
+										if (addr.equals(a_list.get(i))) {
 											found = true;
 											break;
 										}
-										current = a_list.get(i);
 									}//
 									if (found == false){ //if address was not found in list then add it
 										a_list.add(addr);
 									}
 								}
+								mergeCount ++; //number times clusters merged
+								break;
 							}
 						}catch (ScriptException e){
 							//tx_ilist.remove(tx_input);  Would like to remove but doesn't work
+							exc = true;
 							cluster_in_map = true;
+							eCount++; //num exceptions
 							break;
 						}
-							
+					
 					}//end i_list loop
 					
 					/* if cluster is not in cluster_map */
-					if (cluster_in_map == false && tx_ilist.size() > 1){
+					if (cluster_in_map == false){
+						
 						Address key = tx_ilist.get(0).getFromAddress();
 						LinkedList<Address> new_cluster = new LinkedList();
+						new_cluster.add(key);
 						
 						/* Add input addresses to new cluster */
-						for (TransactionInput txi : tx_ilist){
-							Address new_addr = txi.getFromAddress();
-							new_cluster.add(new_addr);
+						for (int i = 1; i < tx_ilist.size(); i++){
+							Address new_addr = tx_ilist.get(i).getFromAddress();
+							
+							/* Check for duplicate addresses */
+							boolean in_list = false;
+							for (Address j: new_cluster){
+								if (j.equals(new_addr)){
+									in_list = true;
+									break;
+								}
+							}
+							/* address not already in list so add */
+							if (in_list == false){
+								new_cluster.add(new_addr);
+							}
 						}
 						
-						/* Add <Key, Cluster> to cluster_map */
-						cluster_map.put(key, new_cluster);
+						/* Add <Key, Cluster> to cluster_map if size > 1*/
+						if (new_cluster.size() > 1) {
+						
+							cluster_map.put(key, new_cluster);
+							
+							if (new_cluster.size() > largestCluster){
+								largestCluster = new_cluster.size();
+							}
+						}
+						
+						
 						
 					}
 				}
 			}
 			stored_block = stored_block.getPrev(kit.store());
-		}//counter loop
+		}//end counter loop 
 		Set<Address> keyset = cluster_map.keySet();
 		Collection<LinkedList<Address>> values = cluster_map.values();
-		System.out.println(values.size());
-		System.out.println(keyset);
+		Address[] keyArray = keyset.toArray(new Address[keyset.size()]);
+		Object[] vArray = values.toArray();
+		hwriter.println("*******************CLUSTERS**************************");
+		for (int i = 0; i< keyArray.length; i ++){
+			hwriter.println(keyArray[i]);
+			hwriter.println(vArray[i]);
+			hwriter.println("************************************");
+		}
+		System.out.println("keys: " + keyArray.length);
+		System.out.println("Num Clusters: " + vArray.length);
+		System.out.println("Total Tx: " + totalTx);
+		System.out.println("%Tx used: " + (totalTx-eCount)/totalTx);
+		System.out.println("Clusters merged: " + mergeCount);
+		System.out.println("Exceptions: " + eCount);
 	}//end main
 
 }
