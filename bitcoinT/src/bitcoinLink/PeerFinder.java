710553b78060ee9bc0a09c1246e49ff4a22233e8
Connor Van Drisse <vandr042@umn.edu>
2015-10-15 22:31:05 -0500

    prototype PeerFinder

--- bitcoinT/src/bitcoinLink/PeerFinder.java
+++ bitcoinT/src/bitcoinLink/PeerFinder.java
@@ -8,36 +8,66 @@ import org.bitcoinj.utils.BriefLogFormatter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.PrintStream;
+import java.net.InetAddress;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
-public class PeerFinder {
-
-	private static NetworkParameters params = MainNetParams.get();
+public class PeerFinder implements Runnable{
 	
-	private static HashMap<String,String> findPeers(){
-		HashMap <String, String> peers = new HashMap();
-		WalletAppKit kit = new WalletAppKit(params, new File("foo"), "test");
-		kit.startAsync();
-		kit.awaitRunning();
-		PeerGroup pGroup = kit.peerGroup();
-		List<Peer> pl = pGroup.getConnectedPeers();
-		
-		
-		return peers;
-	}
+	static NetworkParameters params = MainNetParams.get();
+	static HashMap <InetAddress, Long> peers = new HashMap();
+	static WalletAppKit kit = new WalletAppKit(params, new File("foo"), "test");
+	static PeerGroup pGroup;
+	static List<Peer> pl;
+	File file1 = new File("connections.txt");
+	static PrintStream writer;
+	static Thread tthread;
 	
-	public static void main(String[] args) {
-		WalletAppKit kit = new WalletAppKit(params, new File("foo"), "test");
-		kit.startAsync();
-		kit.awaitRunning();
-		PeerGroup pGroup = kit.peerGroup();
-		List<Peer> pl = pGroup.getConnectedPeers();
-		pl.get(0).getAddr().addListener(arg0, arg1);
-		
-
+	/* constructor */
+	public PeerFinder(){
 	}
 
+	/* initializes peergroup and starts main thread that will start the helper threads */
+	public void findPeers() throws FileNotFoundException, InterruptedException{
+		writer = new PrintStream("connections.txt");
+		kit.startAsync();
+		kit.awaitRunning();
+		pGroup = kit.peerGroup();
+		pl = pGroup.getConnectedPeers();
+		
+	}
+	
+	/* this is where the helper threads get created */
+	public void run() {
+		
+		for (Peer peer: pl){
+			try {
+				PeerHelper newHelper = new PeerHelper(peer, peers);
+				Thread pthread = new Thread(newHelper);
+				pthread.start();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		try {
+			Thread.sleep(20000);
+			tthread.notify();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+		PeerFinder finder = new PeerFinder();
+		finder.findPeers();
+		tthread = new Thread(finder);
+		tthread.setDaemon(true);
+		tthread.start();
+		finder.wait();
+		System.out.println(peers);
+		
+	}
 
 }
--- bitcoinT/src/bitcoinLink/PeerHelper.java
+++ bitcoinT/src/bitcoinLink/PeerHelper.java
@@ -2,24 +2,81 @@ package bitcoinLink;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.*;
+import org.bitcoinj.core.*;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.store.*;
+import org.bitcoinj.utils.BriefLogFormatter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.InetAddress;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 public class PeerHelper implements Runnable {
 	
-	public PeerHelper(HashMap hm){
-		
+	static HashMap<InetAddress, Long> addrMap;
+	static Peer aPeer;
+	File file1 = new File("connections.txt");
+	static PrintStream writer;
+	static PrintWriter writer1;
+	static AddressMessage message;
+	
+	
+	public PeerHelper(Peer peer, HashMap hm) throws FileNotFoundException{
+		addrMap = hm;
+		aPeer = peer;
+		writer = new PrintStream("connections.txt");
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
 		
+		/*keep asking peers for addresses until main thread in peerFinder terminates */
+		while(true){
+		/* not sure what to do with an exception */
+			try {
+				message = aPeer.getAddr().get();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+			
+			/* get inetAdress and check if in map, if not add else update if time updated */
+			List<PeerAddress> addresses = message.getAddresses();
+			for (PeerAddress addr: addresses){
+				InetAddress inetAddr = addr.getAddr();
+				writer.println(addr);
+				writer.println("Trying to get inetADDR: " + addrMap.get(inetAddr));
+				if (addrMap.get(inetAddr) == null){
+					addrMap.put(inetAddr, addr.getTime());
+					writer.println("Peer" + inetAddr + ": " + addr.getTime());
+					writer.println("Peer" + inetAddr + ": " + addr.getTime());
+				}else{
+					Long time = addr.getTime();
+					if (time > addrMap.get(inetAddr)){
+						writer.println("Peer" + inetAddr + ": " + time);
+						System.out.println(time);
+					}
+				}
+			}
+			try {
+				Thread.sleep(30000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			writer.close();
+		}
+	
 	}
 
 }
