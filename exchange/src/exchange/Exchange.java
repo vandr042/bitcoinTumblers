515b7b1874ad42dcc0c1bbccc80c349438c2557f
Myles <Myles@192.168.0.103>
2015-11-05 23:40:25 -0600

    Created class Trade and Object. Currently only contain data structures and instantiation method.

--- exchange/src/exchange/Exchange.java
+++ exchange/src/exchange/Exchange.java
@@ -1,4 +1,5 @@
 package exchange;
+
 import java.net.*;
 import java.io.*;
 
@@ -9,17 +10,7 @@ public final class Exchange {
 		
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-		// optional default is GET
 		con.setRequestMethod("GET");
-
-		//add request header
-		con.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
 		BufferedReader in = new BufferedReader(
 		        new InputStreamReader(con.getInputStream()));
 		String inputLine;
@@ -29,8 +20,6 @@ public final class Exchange {
 			response.append(inputLine);
 		}
 		in.close();
-
-		//print result
 		System.out.println(response.toString());
     }
 }
