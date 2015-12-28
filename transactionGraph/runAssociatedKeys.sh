89a7a89ca081333255ddfc642b40312091170e75
Max Schuchard <schuch@cs.umn.edu>
2015-12-28 12:54:50 -0600

    Updated run script.

--- transactionGraph/runAssociatedKeys.sh
+++ transactionGraph/runAssociatedKeys.sh
@@ -6,6 +6,6 @@ JAR_PATH=$JAR_DIR"bitcoinj.jar:"$JAR_DIR"slf4j.jar:"$JAR_DIR"commonsLogging.jar"
 #TODO might need a bin dir first
 rm -rf bin/
 mkdir bin/
-javac -cp $JAR_PATH:src/ -d bin/ src/bitcoinLink/FindAddress.java
+javac -cp $JAR_PATH:src/ -d bin/ src/bitblender/GetPool.java
 #TODO update mem to your system's size
-java -cp $JAR_PATH:bin/ -Xmx4G -d64 bitcoinLink.FindAddress $1
+java -cp $JAR_PATH:bin/ -Xmx32G -d64 bitblender.GetPool
