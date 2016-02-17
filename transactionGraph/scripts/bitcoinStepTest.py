3366a9475d78a7782606f4092a3302c1d1f8a4d2
Connor Van Drisse <vandr042@umn.edu>
2016-02-17 10:30:16 -0600

    threaded

--- transactionGraph/scripts/bitcoinStepTest.py
+++ transactionGraph/scripts/bitcoinStepTest.py
@@ -1,5 +1,6 @@
 import bitcoinBloomTest
 import numpy
+import threading
 class StepTest:
     def __init__(self,poolPercentage, stepSize):
         self.step = stepSize
@@ -13,28 +14,38 @@ class StepTest:
             print(i)
 
     def writeResults(self):
-        f = open('bloomTestStats', 'w')
+        f = open('bloomTestStats.txt', 'w')
         f.write("(testSize,[mean rounds, std, median])\n")
         for i in self.testResults.items():
-            f.write(i)
+            f.write(str(i))
             f.write("\n")
+        f.close()
 
-        
+    def threadFunc(self,numAddr):
+        roundsList = []
+        for i in range(0,1000):
+            test = bitcoinBloomTest.BitcoinBloomTest(self.goalPercentage, numAddr)
+            roundsList.append(test.runTest())
+        meanRounds = numpy.mean(roundsList)
+        std = numpy.std(roundsList)
+        median = numpy.std(roundsList)
+        self.testResults[numAddr] = [meanRounds,std,median]
+        return
 
     def runTest(self):
+        threads = []
         while (self.numAddresses <= 600000):
-            roundsList = []
-            for i in range(0,1000):
-                test = bitcoinBloomTest.BitcoinBloomTest(self.goalPercentage, self.numAddresses)
-                roundsList.append(test.runTest())
-            meanRounds = numpy.mean(roundsList)
-            std = numpy.std(roundsList)
-            median = numpy.std(roundsList)
-            self.testResults[self.numAddresses] = [meanRounds,std,median]
+            threads.append(threading.Thread(target = self.threadFunc, args = (self.numAddresses,)))
+            threads[len(threads)-1].start()
             self.numAddresses += self.step
+        for i in threads:
+            i.join()
+        return
+
+
+    
                 
 x = StepTest(90,25000)
 x.runTest()
-x.printResults()
 x.writeResults()
 
