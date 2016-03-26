6f931e982c7c245f67ee03f180369b2dc6e663c1
pendgaft <schuch@cs.umn.edu>
2016-03-26 16:10:38 -0500

    pushed in updates to the run script

--- ipLinking/runSpy.sh
+++ ipLinking/runSpy.sh
@@ -5,5 +5,6 @@ JARPATH="../jars"
 rm -rf bin/
 mkdir bin/
 javac -cp $JARPATH/commonsLogging.jar:$JARPATH/mjsBCJ.jar:$JARPATH/slf4j.jar:src/ -d bin/ src/control/Manager.java
+echo "done compiling"
 #TODO update mem to your system's size
-java -cp $JARPATH/commonsLogging.jar:$JARPATH/mjsBCJ.jar:$JARPATH/slf4j.jar:bin/ -Xmx50G -d64 control.Manager
+java -cp $JARPATH/commonsLogging.jar:$JARPATH/mjsBCJ.jar:$JARPATH/slf4j.jar:bin/ -Xmx18G -d64 control.Manager
