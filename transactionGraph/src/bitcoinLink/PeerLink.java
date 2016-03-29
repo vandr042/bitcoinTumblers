948fc665b9b2c1a583fc0997e581e58f8b48f4d2
pendgaft <schuch@cs.umn.edu>
2016-03-29 11:19:13 -0500

    slight cleaning to peer linker to make it a bit more user friendly

--- transactionGraph/src/bitcoinLink/PeerLink.java
+++ transactionGraph/src/bitcoinLink/PeerLink.java
@@ -1,4 +1,5 @@
 package bitcoinLink;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,100 +13,119 @@ import java.util.LinkedList;
 import java.util.Set;
 import java.util.regex.*;
 
-
 public class PeerLink {
-	private HashMap<String, HashSet<String>> pMap; //(Peer, peers connected to) pairs
-	private HashMap<String, LinkedList<TStampPeerPair>> txMap; //Mapping from txID to (peer,timestamp peer says it saw tx) pairs 
-	File f = new File("simtest.log");
-	BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-	public PeerLink(String dataFile) throws IOException{
+
+	/**
+	 * (Peer, peers connected to) pairs
+	 */
+	private HashMap<String, HashSet<String>> pMap;
+
+	/**
+	 * Mapping from txID to (peer,timestamp peer says it saw tx) pairs
+	 */
+	private HashMap<String, LinkedList<TStampPeerPair>> txMap;
+
+	public PeerLink(String dataFile) throws IOException {
 		pMap = new HashMap<String, HashSet<String>>();
 		txMap = new HashMap<String, LinkedList<TStampPeerPair>>();
 		buildMapsFromFile(dataFile);
 	}
-	
-	/* sim calls findSender on each transaction in txMap and returns a map from txID's to the set of possible senders */
-	public HashMap<String, HashSet<String>> sim(int searchDepth) throws IOException{
+
+	/*
+	 * sim calls findSender on each transaction in txMap and returns a map from
+	 * txID's to the set of possible senders
+	 */
+	public HashMap<String, HashSet<String>> sim(int searchDepth) throws IOException {
 		System.out.println("Starting simulation with search depth: " + searchDepth);
 		HashMap<String, HashSet<String>> txToPeersMap = new HashMap<String, HashSet<String>>();
 		Set<String> txIDs = txMap.keySet();
 		String[] txArr = new String[txIDs.size()];
 		txIDs.toArray(txArr);
-		for (String tx:txArr){
-			//bw.write("tx: " + tx + "\n");
-			txToPeersMap.put(tx, this.findSender(tx,searchDepth));
+		for (String tx : txArr) {
+			// bw.write("tx: " + tx + "\n");
+			txToPeersMap.put(tx, this.findSender(tx, searchDepth));
 		}
-		//bw.close();
+		// bw.close();
 		return txToPeersMap;
 	}
 
-	/* findSender starts at the earliest known timestamp and iterates to a specified search depth, intersecting the peerConn sets */
-	private HashSet<String> findSender(String tx, int searchDepth) throws IOException{
+	/*
+	 * findSender starts at the earliest known timestamp and iterates to a
+	 * specified search depth, intersecting the peerConn sets
+	 */
+	private HashSet<String> findSender(String tx, int searchDepth) throws IOException {
 		LinkedList<TStampPeerPair> tsppList = txMap.get(tx);
 		Collections.sort(tsppList);
 		TStampPeerPair tspp = tsppList.get(0);
 		String peer = tspp.getPeer();
 		HashSet<String> peerConns = pMap.get(peer);
 		HashSet<String> intersectConns = (HashSet<String>) peerConns.clone();
-		HashSet<String> trailIConns = (HashSet<String>) intersectConns.clone(); //in case we reach 0 peers in the set
-		for (int i = 1; i < searchDepth; i++){
+		// HashSet<String> trailIConns = (HashSet<String>)
+		// intersectConns.clone();
+	
+		for (int i = 1; i < searchDepth; i++) {
 			TStampPeerPair tmpTSPP = tsppList.get(i);
 			String tmpPeer = tmpTSPP.getPeer();
 			HashSet<String> tmpPeerConns = pMap.get(tmpPeer);
 			intersectConns.retainAll(tmpPeerConns);
-			if (intersectConns.size() > 1){
-				trailIConns = (HashSet<String>) intersectConns.clone();
-			}else{
-				break;
-			}
+			// if (intersectConns.size() > 1) {
+			// trailIConns = (HashSet<String>) intersectConns.clone();
+			// } else {
+			// break;
+			// }
 		}
-		//bw.write("intersected set: " + intersectConns.toString() + "\n");
-		if (intersectConns.size() == 0){ //step back
-			return trailIConns; 
-		}else{
-			return intersectConns;
-		}
+		// bw.write("intersected set: " + intersectConns.toString() + "\n");
+		// if (intersectConns.size() == 0) { // step back
+		// return trailIConns;
+		// } else {
+		// return intersectConns;
+		// }
+		return intersectConns;
 	}
-	
-	private void buildMapsFromFile(String filename) throws IOException{
+
+	private void buildMapsFromFile(String filename) throws IOException {
 		File f = new File(filename);
 		BufferedReader reader = new BufferedReader(new FileReader(f));
-		
+
 		String regex = "(\\w*),(?:(\\d*),)*\\[((?:\\d+\\.*){4})+\\]:\\d*(?:,\\[((?:\\d+\\.*){4})+\\]:\\d*)*,(\\d*)";
 		Pattern p = Pattern.compile(regex);
-	
+
 		/* pattern match each line to build peer map */
 		System.out.println("Building maps from file...");
 		String line = reader.readLine();
-		while (line != null){
+		while (line != null) {
 			Matcher m = p.matcher(line);
 			m.matches();
-			String msgType = m.group(1); //conn, remoteconn, or tx
-			String txID = m.group(2); //Can be null
-			String addr = m.group(3); //This is the address that initiates a connection in remote connection messages
-			String toAddr = m.group(4); //Peer that above addr connected to.  Can be null
+			String msgType = m.group(1); // conn, remoteconn, or tx
+			String txID = m.group(2); // Can be null
+			String addr = m.group(3); // This is the address that initiates a
+										// connection in remote connection
+										// messages
+			String toAddr = m.group(4); // Peer that above addr connected to.
+										// Can be null
 			String timeStamp = m.group(5);
-			/*System.out.println("mtype: " + m.group(1));
-			System.out.println("txID: " + m.group(2));
-			System.out.println("addr: " + addr);
-			System.out.println("toAddr: " + toAddr);
-			System.out.println("time stamp: " + timeStamp);*/
-			
+			/*
+			 * System.out.println("mtype: " + m.group(1)); System.out.println(
+			 * "txID: " + m.group(2)); System.out.println("addr: " + addr);
+			 * System.out.println("toAddr: " + toAddr); System.out.println(
+			 * "time stamp: " + timeStamp);
+			 */
+
 			/* populate maps based on message type */
 			HashSet<String> peerConns;
-			if (msgType.compareTo("conn") == 0){
+			if (msgType.compareTo("conn") == 0) {
 				peerConns = new HashSet<String>();
 				peerConns.add(addr);
 				pMap.put(addr, peerConns);
-			}else if(msgType.compareTo("remoteconn") == 0){
+			} else if (msgType.compareTo("remoteconn") == 0) {
 				peerConns = pMap.get(toAddr);
 				peerConns.add(addr);
-			}else{
+			} else {
 				LinkedList<TStampPeerPair> tsppList = txMap.get(txID);
-				if (tsppList != null){
-					TStampPeerPair tspp = new TStampPeerPair(timeStamp,addr);
+				if (tsppList != null) {
+					TStampPeerPair tspp = new TStampPeerPair(timeStamp, addr);
 					tsppList.add(tspp);
-				}else{
+				} else {
 					tsppList = new LinkedList<TStampPeerPair>();
 					TStampPeerPair tspp = new TStampPeerPair(timeStamp, addr);
 					tsppList.add(tspp);
@@ -117,20 +137,21 @@ public class PeerLink {
 		reader.close();
 		System.out.println("Finished building maps...");
 	}
-	
-	public HashMap<String, HashSet<String>> getPmap(){
+
+	public HashMap<String, HashSet<String>> getPmap() {
 		return pMap;
 	}
-	public HashMap<String, LinkedList<TStampPeerPair>> getTxMap(){
+
+	public HashMap<String, LinkedList<TStampPeerPair>> getTxMap() {
 		return txMap;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		PeerLink pl = new PeerLink("/home/connor/workspace/bitcoinTumblers/miscScripts/peer-finder-synth-out.log");
 		pl.sim(2);
-		//System.out.println(pl.pMap.keySet().toString());
-		//System.out.println(pl.txMap.keySet().toString());
-		//System.out.println(pl.pMap.values().toString());
+		// System.out.println(pl.pMap.keySet().toString());
+		// System.out.println(pl.txMap.keySet().toString());
+		// System.out.println(pl.pMap.values().toString());
 
 	}
 
--- transactionGraph/src/bitcoinLink/PeerLinkTester.java
+++ transactionGraph/src/bitcoinLink/PeerLinkTester.java
@@ -14,96 +14,113 @@ import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PeerLinkTester{
+public class PeerLinkTester {
 	/* depth of ~275 to get to 1 peer */
-	private HashMap<String, String> truthTxToPeer = new HashMap<String,String>(); 
+	private HashMap<String, String> truthTxToPeer = new HashMap<String, String>();
 	private int depthStep;
 	private PeerLink peerLink;
 	private int maxDepth;
-	public PeerLinkTester(int stepSize, PeerLink pl, int depth, String truthFile) throws IOException{
+
+	public PeerLinkTester(int stepSize, PeerLink pl, int depth, String truthFile) throws IOException {
 		depthStep = stepSize;
 		peerLink = pl;
 		maxDepth = depth;
 		buildMapFromTruth(truthFile);
 	}
-	
-	/* testForBestDepth runs PeerLink.sim for a specified number of different depths and outputs the accuracies to file */
-	public void testForBestDepth() throws IOException{
+
+	/*
+	 * testForBestDepth runs PeerLink.sim for a specified number of different
+	 * depths and outputs the accuracies to file
+	 */
+	public void testForBestDepth(String outFile) throws IOException {
 		System.out.println("********************************** Testing for Depth ************************************");
-		File f = new File("peerLinkSizeTest.log");
+		File f = new File(outFile);
 		BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-		for (int searchDepth = depthStep; searchDepth < maxDepth; searchDepth += depthStep){
-			float avgSetSize,accuracy, totalSets, numPeers;
+		for (int searchDepth = depthStep; searchDepth < maxDepth; searchDepth += depthStep) {
+			double avgSetSize, accuracy, totalSets, numPeers, nonZeroCount;
 			numPeers = 0;
-			HashMap <String, HashSet<String>> txToPeers = peerLink.sim(searchDepth);
+			nonZeroCount = 0;
+			HashMap<String, HashSet<String>> txToPeers = peerLink.sim(searchDepth);
 			Collection<HashSet<String>> peers = txToPeers.values();
-			for (HashSet<String> pset:peers){
+			for (HashSet<String> pset : peers) {
 				numPeers += pset.size();
+				if (pset.size() > 0) {
+					nonZeroCount += 1.0;
+				}
 			}
 			totalSets = peers.size();
-			avgSetSize = numPeers/totalSets;
+			avgSetSize = numPeers / totalSets;
 			accuracy = this.checkAccuracy(txToPeers);
-			bw.write("Depth: " + searchDepth + " Accuracy: " + accuracy + " avgSetSize: " + avgSetSize + "\n");
+			bw.write("Depth: " + searchDepth + " Accuracy: " + accuracy + " avgSetSize: " + avgSetSize
+					+ " non zero avg set size: " + (numPeers / nonZeroCount) + "\n");
 		}
 		bw.close();
 	}
-	
+
 	/* private helper */
-	private float checkAccuracy(HashMap<String,HashSet<String>> txPeerMap){
+	private float checkAccuracy(HashMap<String, HashSet<String>> txPeerMap) {
 		float accuracy, totalTx, correct;
 		correct = 0;
 		String[] txArr = new String[txPeerMap.keySet().size()];
 		txPeerMap.keySet().toArray(txArr);
 		totalTx = txArr.length;
-		for (String tx:txArr){
+		for (String tx : txArr) {
 			String peer = truthTxToPeer.get(tx);
 			HashSet<String> possiblePeers = txPeerMap.get(tx);
-			if (possiblePeers.contains(peer) == true){
+			if (possiblePeers.contains(peer) == true) {
 				correct += 1;
 			}
 		}
-		accuracy = correct/totalTx;
+		accuracy = correct / totalTx;
 		return accuracy;
 	}
-	
+
 	/* called in constructor to build truth map */
-	private void buildMapFromTruth(String filename) throws IOException{
+	private void buildMapFromTruth(String filename) throws IOException {
 		File f = new File(filename);
 		BufferedReader reader = new BufferedReader(new FileReader(f));
-		
+
 		String regex = "\\[((?:\\d+\\.*){4})+\\]:\\d*,(\\d*),(\\d*)";
 		Pattern p = Pattern.compile(regex);
-	
+
 		/* pattern match each line to build tx to peer map */
 		System.out.println("Building maps from file...");
 		String line = reader.readLine();
-		while (line != null){
+		while (line != null) {
 			Matcher m = p.matcher(line);
 			m.matches();
 			String addr = m.group(1);
 			String txID = m.group(2);
 			String timeStamp = m.group(3);
-			
-			/*System.out.println("txID: " + txID);
-			System.out.println("addr: " + addr);
-			System.out.println("time stamp: " + timeStamp);*/
-			
+
+			/*
+			 * System.out.println("txID: " + txID); System.out.println("addr: "
+			 * + addr); System.out.println("time stamp: " + timeStamp);
+			 */
+
 			/* populate maps based on message type */
 			truthTxToPeer.put(txID, addr);
 			line = reader.readLine();
 		}
 		reader.close();
-		//System.out.println("Finished building maps...");
+		// System.out.println("Finished building maps...");
 	}
-	
-	public HashMap<String, String> getTruthMap(){
+
+	public HashMap<String, String> getTruthMap() {
 		return this.truthTxToPeer;
 	}
-	
-	public static void main(String[] args) throws IOException{
-		PeerLink pl = new PeerLink("/home/connor/workspace/bitcoinTumblers/miscScripts/peer-finder-synth-out.log");
-		PeerLinkTester test = new PeerLinkTester(1,pl,1000, "/home/connor/workspace/bitcoinTumblers/miscScripts/peer-finder-synth-groundTruth.log");
-		test.testForBestDepth();
-		
+
+	private static void gogo(String fileBase) throws IOException {
+		PeerLink pl = new PeerLink("../miscScripts/" + fileBase + "-out.log");
+		PeerLinkTester test = new PeerLinkTester(1, pl, 10, "../miscScripts/" + fileBase + "-groundTruth.log");
+		test.testForBestDepth(fileBase.split("-")[0] + ".txt");
+	}
+
+	public static void main(String[] args) throws IOException {
+//		gogo("peer-finder-synth");
+//		gogo("allKnown-peer-finder-synth");
+//		gogo("perfect-peer-finder-synth");
+//		gogo("zeroRandom-peer-finder-synth");
+		gogo("realDelay-peer-finder-synth");
 	}
 }
