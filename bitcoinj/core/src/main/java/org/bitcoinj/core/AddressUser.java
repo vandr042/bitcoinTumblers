a8e1c8913b28abae8ad15e143c7bfe50ca8d7cab
pendgaft <schuch@cs.umn.edu>
2016-04-26 12:42:20 -0500

    Added the ability to phone transactions home.

--- bitcoinj/core/src/main/java/org/bitcoinj/core/AddressUser.java
+++ bitcoinj/core/src/main/java/org/bitcoinj/core/AddressUser.java
@@ -1,7 +1,11 @@
 package org.bitcoinj.core;
 
+import java.util.List;
+
 public interface AddressUser {
 	
 	public void getAddresses(AddressMessage m, Peer rcvingPeer);
+	
+	public void getInventory(List<InventoryItem> incTx, Peer rcvingPeer);
 
 }
