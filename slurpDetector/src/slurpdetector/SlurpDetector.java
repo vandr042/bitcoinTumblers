85139a89fa8df9e0852ceaa71116b4b5bfa0703b
Myles <Myles@172.19.5.114>
2016-05-16 13:37:09 -0500

    Added functionality to the slurp detective.

--- slurpDetector/src/slurpdetector/SlurpDetector.java
+++ slurpDetector/src/slurpdetector/SlurpDetector.java
@@ -4,11 +4,15 @@ public class SlurpDetector {
     
     public static void main(String[] args) {
         Detective sherlock = new Detective();
-        sherlock.importData("data/iplinks.csv");
-        sherlock.invertData();
-        //Max's Bitcoin Slurp Tx
-        String tempTx = "68d2a1cd56e7f2ac1a7d14069650bb8bf786c30b94fe2e981b838c82f25b280a";//"5756ff16e2b9f881cd15b8a7e478b4899965f87f553b6210d0f8e5bf5be7df1d";
+        sherlock.importData("data/iplinks.csv"); //imports Connor's data into a list of IPLink objects
+        sherlock.invertData(); //inverts the TxId, and IP from Connor's data
+        
+        //sherlock.startAnalyzing();
+        
+        String tempTx = "68d2a1cd56e7f2ac1a7d14069650bb8bf786c30b94fe2e981b838c82f25b280a"; //Max's Bitcoin Slurp Tx
         sherlock.callBlockExplorer(tempTx);
+        
+        //sherlock.matchIPAddresses();
     }
 
 }
