d3cef7bc3c72c7a7a62f2057e2888fa1a9330d4c
Connor S VanDrisse <vandr042@cs-kinakuta.cs.umn.edu>
2016-06-22 11:07:44 -0500

    parser to determine how often the first peer has the earliest timestamp

--- miscScripts/vpParser.py
+++ miscScripts/vpParser.py
@@ -1,27 +1,39 @@
 import numpy as np
 
-f = open('5VP.txt', 'r')
-
-
+f = open('25VP.txt', 'r')
 
 i = 0
 tstamps = []
 addrs = []
+firstTen = []
 conn_dict = {}
+numDirFirst = 0.0 # Number of times the first directly connected peer has the 
+		# earliest time stamp
 for line in f:
 	if (line != "**\n"):
 		x = line.split(' ')
 		if (i < 10):	# First ten
-			pass	
+			firstTen.append(x[1])
 		else:
 			tstamps.append(int(x[2].rstrip()))
 			addrs.append(x[1])
 		i = i + 1	
 	else:
-		conns = zip(tstamps, addrs)
-		conn_dict = dict(conns)
+		# loop over tstamps/addrs adding them to the dict if the
+		# key does not exist, and handling collisions using 
+		# lists of addrs as the values
+		for j in range(0, len(tstamps)):
+			if tstamps[j] in conn_dict:
+				conn_dict[tstamps[j]].append(addrs[j])
+			else:
+				conn_dict[tstamps[j]] = [addrs[j]]
 		tstamps.sort()
+		for addr in conn_dict[tstamps[0]]:
+			if addr == firstTen[0]:
+				numDirFirst = numDirFirst + 1	
 		i = 0
 		tstamps = []
 		addrs = []
+		firstTen = []
 		conn_dict = {}
+print(str(numDirFirst/1000))
