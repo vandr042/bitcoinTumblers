95abfa6d8118d1c87410e0223e5fe48f7f5b33ac
Connor S VanDrisse <vandr042@cs-kinakuta.cs.umn.edu>
2016-06-22 11:47:44 -0500

    added code that attempts to recover the first ten messages

--- miscScripts/vpParser.py
+++ miscScripts/vpParser.py
@@ -9,6 +9,7 @@ firstTen = []
 conn_dict = {}
 numDirFirst = 0.0 # Number of times the first directly connected peer has the 
 		# earliest time stamp
+correctness = 0.0
 for line in f:
 	if (line != "**\n"):
 		x = line.split(' ')
@@ -30,10 +31,35 @@ for line in f:
 		tstamps.sort()
 		for addr in conn_dict[tstamps[0]]:
 			if addr == firstTen[0]:
-				numDirFirst = numDirFirst + 1	
+				numDirFirst = numDirFirst + 1
+
+		# try to build the first ten messages
+		myFirstTen = []
+		trail = conn_dict[tstamps[0]][0]
+		myFirstTen.append(trail)
+		k = 1
+		j = 0
+		while k < 10 and j < len(tstamps):
+			t = tstamps[j]
+			for addr in conn_dict[t]:
+				if trail != addr:
+					myFirstTen.append(addr)
+					k = k + 1	
+					trail = addr
+			j = j+1
+		numFirstTenCorrect = 0.0
+		for j in range(0,len(firstTen)):
+			if (myFirstTen[j] == firstTen[j]):
+				numFirstTenCorrect = numFirstTenCorrect + 1
+		print("myFirstTen length: " + str(len(myFirstTen)))
+		print("correctness: " + str(numFirstTenCorrect/10))	
+		correctness += numFirstTenCorrect/10	
+		numFirstTenCorrect = 0.0	
+		#reset variables/data structs for next transaction
 		i = 0
 		tstamps = []
 		addrs = []
 		firstTen = []
 		conn_dict = {}
 print(str(numDirFirst/1000))
+print(str(correctness/1000))
