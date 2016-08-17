8a0dbc79132c843e3e2d7671697d8af4578b2837
pendgaft <m.schuchard@gmail.com>
2016-08-17 12:14:16 -0400

    Removed use of threads in TimeRotatingHashMap.
    This results in the memory freeing being lazy (triggered on put) but
    removes excessive threads that prevented the use of thousands of these
    data structures.

--- ipLinking/src/data/TimeRotatingHashMap.java
+++ ipLinking/src/data/TimeRotatingHashMap.java
@@ -1,14 +1,16 @@
 package data;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 
-public class TimeRotatingHashMap<K, V> implements Runnable {
+public class TimeRotatingHashMap<K, V> {
 
 	private HashMap<K, V> currentGen;
 	private HashMap<K, V> oldGen;
 
+	private long lastClean;
 	private long cleanIntervalMS;
-	private Thread myThread;
 
 	public static final long DEFAULT_CLEAN_INTERVAL_MS = 600000;
 
@@ -19,61 +21,49 @@ public class TimeRotatingHashMap<K, V> implements Runnable {
 	public TimeRotatingHashMap(long cleanInterval) {
 		this.currentGen = new HashMap<K, V>();
 		this.oldGen = new HashMap<K, V>();
+		this.lastClean = System.currentTimeMillis();
 		this.cleanIntervalMS = cleanInterval;
-
-		this.myThread = new Thread(this);
-		this.myThread.setDaemon(true);
-		this.myThread.setName("Rot Hash Cleaner");
-		this.myThread.start();
 	}
 
-	public void run() {
-
-		try {
-			while (true) {
-				Thread.sleep(this.cleanIntervalMS);
-				
-				synchronized(this){
-					this.oldGen = this.currentGen;
-					this.currentGen = new HashMap<K, V>();
-				}
-			}
-		} catch (InterruptedException e) {
-			//Currently do nothing, just end
-		}
-	}
-
-	public Thread getCleanerThreadRef() {
-		return this.myThread;
-	}
-
-	public void put(K theKey, V theValue){
-		synchronized(this){
-			if(this.oldGen.containsKey(theKey)){
-				this.oldGen.remove(theKey);
-			}
-			this.currentGen.put(theKey, theValue);
-		}
-	}
-	
-	public V get(K theKey){
-		V retObj = null;
-		synchronized(this){
-			if(this.currentGen.containsKey(theKey)){
-				retObj = this.currentGen.get(theKey);
-			}else if(this.oldGen.containsKey(theKey)){
-				retObj = this.oldGen.get(theKey);
-			}
+	public void put(K theKey, V theValue) {
+		/*
+		 * Rotate generations if we need to
+		 */
+		long time = System.currentTimeMillis();
+		if(time >= this.lastClean + this.cleanIntervalMS){
+			this.oldGen = this.currentGen;
+			this.currentGen = new HashMap<K, V>();
+			this.lastClean = time;
 		}
 		
+		/*
+		 * Move into current gen if it exists in old gen (don't duplicate entries)
+		 */
+		if (this.oldGen.containsKey(theKey)) {
+			this.oldGen.remove(theKey);
+		}
+		this.currentGen.put(theKey, theValue);
+	}
+
+	public V get(K theKey) {
+		V retObj = null;
+		if (this.currentGen.containsKey(theKey)) {
+			retObj = this.currentGen.get(theKey);
+		} else if (this.oldGen.containsKey(theKey)) {
+			retObj = this.oldGen.get(theKey);
+		}
+
 		return retObj;
 	}
+
+	public boolean containsKey(K theKey) {
+		return this.currentGen.containsKey(theKey) || this.oldGen.containsKey(theKey);
+	}
 	
-	public boolean hasKey(K theKey){
-		boolean retVal = false;
-		synchronized(this){
-			retVal = this.currentGen.containsKey(theKey) || this.oldGen.containsKey(theKey);
-		}
-		return retVal;
+	public Set<K> keySet(){
+		HashSet<K> retSet = new HashSet<K>();
+		retSet.addAll(this.currentGen.keySet());
+		retSet.addAll(this.oldGen.keySet());
+		return retSet;
 	}
 }
