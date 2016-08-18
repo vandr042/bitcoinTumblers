3eae235113cd4872d98f1687735be3e475e95c60
pendgaft <m.schuchard@gmail.com>
2016-08-18 14:54:32 -0400

    Added flag for vantage point spy run.

--- ipLinking/runSpy.py
+++ ipLinking/runSpy.py
@@ -13,19 +13,19 @@ BASE_SOURCE_FILE = "src/control/Manager.java"
 BASE_CLASS = "control.Manager"
 JAR_STR = JARPATH + "/commonsLogging.jar:" + JARPATH + "/mjsBCJ.jar:" + JARPATH + "/slf4j.jar:" + JARPATH + "/argparse4j-0.5.0.jar"
 
-def main(plMode):
+def main(plMode, vpMode):
     compile()
     runPrune(plMode)
     if os.listdir("recovery"):
-        runProc = run(True, plMode)
+        runProc = run(True, plMode, vpMode)
     else:
-        runProc = run(False, plMode)
+        runProc = run(False, plMode, vpMode)
     while True:
         runProc.wait()
         print("PROCESS FAILED")
         time.sleep(10)
         print("PROCESS RESTARTED")
-        runProc = run(True, plMode)
+        runProc = run(True, plMode, vpMode)
     
 def compile():
     print("Starting compile")
@@ -44,14 +44,16 @@ def runPrune(plMode):
         procArgs = procArgs + ["--pl"]
     subprocess.Popen(procArgs)
     
-def run(restart, plMode):
+def run(restart, plMode, vpMode):
     procArgs = ["java", "-XX:+UseG1GC","-cp", JAR_STR + ":" + BUILD_DEST, "-Xmx7G", "-d64", BASE_CLASS]
     if plMode:
         procArgs = procArgs + ["--plMan"]
+    if vpMode:
+        procArgs = procArgs + ["--vantagepoint"]
     if restart:
         procArgs = procArgs + ["--recovery"]
     runProc = subprocess.Popen(procArgs)
     return runProc
 
 if __name__ == "__main__":
-    main("--pl" in sys.argv)
+    main("--pl" in sys.argv, "--vp" in sys.argv)
